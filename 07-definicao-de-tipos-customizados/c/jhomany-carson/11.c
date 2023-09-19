958ae7457592b913657f7c5226b4ac816b54d44d
Jhomany Carson <jhomany99@gmail.com>
2023-09-19 00:41:59 -0300

    lista]

--- 07-definicao-de-tipos-customizados/c/jhomany-carson/11.c
+++
@@ -1,3 +0,0 @@
-/*11) Escreva um programa que contenha uma estrutura representando uma data válida. Essa estrutura deve conter os campos dia, mês e
-ano. Em seguida, leia duas datas e armazene nessa estrutura. Calcule e exiba o número de dias que
-decorreram entre as duas datas.*/
