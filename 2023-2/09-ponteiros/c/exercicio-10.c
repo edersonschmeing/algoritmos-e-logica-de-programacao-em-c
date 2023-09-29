690c744e982584a722e4515290e5efb09d6e2874
edersonschmeing <edersonschmeing@gmail.com>
2023-09-29 18:31:45 -0300

    ex 10

---
+++ 2023-2/09-ponteiros/c/exercicio-10.c
@@ -0,0 +1,11 @@
+/*
+10) Considere a seguinte declaração: int a, *b, **c, ***d. 
+
+Escreva um programa que leia a variável a e calcule e exiba o dobro, o triplo e o quádruplo 
+desse valor utilizando apenas os ponteiros b, c e d. 
+
+O ponteiro b deve ser usado para calcular o dobro,
+O ponteiro c deve ser usado para calcular triplo,
+O ponteiro d deve ser usado para calcular quádruplo.
+*/
+
