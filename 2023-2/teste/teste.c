110c4602077a224e18f101ee4e8d01f0898e2f7c
edersonschmeing <edersonschmeing@gmail.com>
2023-11-15 12:06:20 -0300

    teste

--- 2023-2/teste/teste.c
+++ 2023-2/teste/teste.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {  
-    int *p, **r, a = -1, c, b = 10;
+    int *p, **r, a = -2, c, b = 10, d = 1;
     p = &a;
     r = &p;
-    c = **r + ++b;
+    c = **r + ++b + d--;
     printf("%d", c);
 } 
